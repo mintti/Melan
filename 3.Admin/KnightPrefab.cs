a3cdef703b91929ce4fbf5e33298bc370ce3f8dc
Mintti <mintti97@gmail.com>
2019-06-18 15:28:01 +0900

    admin 클릭 시 용병List내 용병 객체

---
+++ 3.Admin/KnightPrefab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KnightPrefab : MonoBehaviour
+{
+    #region  Obj Part
+
+    public Image skin;
+    public Text name;
+    public Text stress;
+
+    #endregion
+
+    public Knight k{get;set;}
+
+    public void SetData(Knight _k)
+    {
+        k = _k;
+
+        skin.sprite = k.skin;
+        name.text = string.Format("{0}", k.name);
+        stress.text = k.stress + "%";      
+    }
+}
