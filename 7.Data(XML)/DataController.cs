c8b5b653e0f1ea3f27391cd8c77736b453a437c4
Mintti <mintti97@gmail.com>
2019-06-18 15:27:18 +0900

    게임 내 데이터 Load 총괄 * 각 영역 내의 데이타를 load하는 함수를 실행시키도록함.

---
+++ 7.Data(XML)/DataController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DataController : MonoBehaviour
+{
+    public UnitData unit;
+    public SkillData skill;
+    public DungeonData dungeon;
+
+    void Start()
+    {
+        Test_InsertData();
+    }
+
+
+    void Test_InsertData(){
+        unit.Test_InsertData();
+        skill.Test_InsertData();
+        dungeon.Test_ConnectData();
+    }
+
+}
