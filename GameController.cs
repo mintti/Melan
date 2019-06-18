20dc5f7b978fd5e87696d9d32fd5fd040c91db26
Mintti <mintti97@gmail.com>
2019-06-18 15:30:40 +0900

    게임 화면 제어 * StartSetting() : 게임 초기화면 제어

---
+++ GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public WorldController world;
+    public AdminController admin;
+
+    void Start()
+    {
+        StartSetting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartSetting()
+    {
+        world.Click();
+        admin.MakeKinghtList();
+    }
+}
