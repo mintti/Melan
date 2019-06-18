18e28028af660dda44fbb8fb55096c09ecfb7874
Mintti <mintti97@gmail.com>
2019-06-18 15:22:41 +0900

    던전 정보. * 일일이 지정해줌. 이름이나 설명은 추후 xml에서 로드해옴.

---
+++ 1.World/Dungeon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Dungeon : MonoBehaviour
+{
+    public int num;
+    public string name;
+    public int level;
+    public int[] monsters;
+    public int[] dangers;
+    public int[] events;
+
+}
