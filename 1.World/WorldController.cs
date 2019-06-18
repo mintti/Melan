ba7f3ec2447291209155031d226ccbe686c7001a
Mintti <mintti97@gmail.com>
2019-06-18 15:25:47 +0900

    Menu - World 이벤트 관리

---
+++ 1.World/WorldController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
+
+public class WorldController : MonoBehaviour
+{
+    public GameObject world;
+    public Button _bt;
+    
+
+    public void Click()
+    {
+        _bt.onClick.Invoke();
+    }
+}
