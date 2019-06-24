20d8f1944d18a13eb027580b890c9a6d08408a1c
Mintti <mintti97@gmail.com>
2019-06-25 00:39:32 +0900

    world map에서 dungeon 시각화 업그레이드. * targetArrow : 화살표 obj생성, EnterTrigger 되면 해당 던전의 자식으로 생성됨. * DungeonExpaln : 해당 던전의 이전 던전 클리어 여부에 따라 (추후 언어별)멘트 출력.

--- 1.World/Collider/DungeonCol.cs
+++ 1.World/Collider/DungeonCol.cs
@@ -1,29 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DungeonCol : MonoBehaviour
 {
     public DungeonInfoGuide guide;
+    public GameObject DungeonExplan;
+    public GameObject targetArrow;
+
+    public string[] ment{get;set;} = new string[2];
+
+    private void Start()
+    {
+
+    }
 
     #region  Trigger영역
     void OnTriggerEnter2D(Collider2D col) {
 
         if(col.tag == "Dungeon")
         {
+            //화살표 생성
+            GameObject obj = Instantiate(targetArrow);
+            obj.transform.SetParent(col.transform, false);
+
+            //해당 던전 정보를 guide에 전달.
             Dungeon _d = col.GetComponent<Dungeon>();
             guide.gameObject.SetActive(true); 
             guide.SetData(_d);
+
+            //던전 출전 가능 여부, 확인.
+            DungeonExplan.SetActive(true);
+            Text explan = DungeonExplan.transform.GetComponentInChildren<Text>();
+            explan.text = ment[_d.isAdmit() ? 1 : 0];
+
         }
     }
 
     void OnTriggerExit2D(Collider2D col) {
 
         if(col.tag == "Dungeon")
-        {
+        {   
+            //화살표 제거
+            Destroy(col.transform.GetChild(0).gameObject);
+
+            //지목된 던전과 guide의 타겟인 던전이 일치?
             Dungeon _d = col.GetComponent<Dungeon>();
             if(_d == guide.d)
+            {
                 guide.gameObject.SetActive(false);    
+                //DungeonExplan 상태 false전환.
+                DungeonExplan.SetActive(false);
+            }
         }
     }
 
