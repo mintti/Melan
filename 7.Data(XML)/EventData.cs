9970a98dfdc299ab651a86069ac095a7fcbaa10b
Mintti <mintti97@gmail.com>
2019-06-25 00:34:19 +0900

    todo 이벤트 정보 저장.

---
+++ 7.Data(XML)/EventData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+delegate void Del();
+
+public class EventData : MonoBehaviour
+{
+    Del todo;
+
+    public static void D_battle()
+    {
+        Debug.Log("전투");
+    }
+    public static void D_day()
+    {
+        Debug.Log("정규 행사");
+    }
+
+
+    private void Start() {
+        
+    }
+
+
+    public void Test_InsertData()
+    {
+        todo = new Del(D_battle);
+        todo += D_day;
+    }
+
+    public void Test_OutputData()
+    {
+        todo();
+    }
+}
