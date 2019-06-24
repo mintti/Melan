6e55f2a42646146e192276ea03b0e4b3c608a3b2
Mintti <mintti97@gmail.com>
2019-06-25 00:34:41 +0900

    추후 언어별로 xml 호출

---
+++ 7.Data(XML)/TextData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextData : MonoBehaviour
+{
+
+    public DungeonCol DungeonCol;
+
+   
+   public void setTextData()
+   {
+       DungeonCol.ment[0] = "출전 불가능. 이전 던전 클리어 필요.";
+       DungeonCol.ment[1] = "출전 가능. 이곳을 눌러 준비해주세요.";
+   }
+}
