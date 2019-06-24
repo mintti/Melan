d240f03d4d961dd2055662ce3c787906cfcd42d4
Mintti <mintti97@gmail.com>
2019-06-25 00:33:41 +0900

    * EventData, TextData 추가. * 테스트 정보 출력 함수 생성.

--- 7.Data(XML)/DataController.cs
+++ 7.Data(XML)/DataController.cs
@@ -8,6 +8,9 @@ public class DataController : MonoBehaviour
     public UnitData unit;
     public SkillData skill;
     public DungeonData dungeon;
+    public EventData event_;
+
+    public TextData text;
 
     void Start()
     {
@@ -15,10 +18,20 @@ public class DataController : MonoBehaviour
     }
 
 
-    void Test_InsertData(){
+    void Test_InsertData()
+    {
         unit.Test_InsertData();
         skill.Test_InsertData();
         dungeon.Test_ConnectData();
+        event_.Test_InsertData();
+
+
+        text.setTextData();
+    }
+
+    void Test_OutPutData()
+    {
+        event_.Test_OutputData();
     }
 
 }
