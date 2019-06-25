134e10d631cb90da2acb7ad31546ed3f309be65d
Mintti <mintti97@gmail.com>
2019-06-26 02:04:41 +0900

    World - Dungeon - KnightList 인자

---
+++ 1.World/WorldKnightPrefab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldKnightPrefab : MonoBehaviour
+{
+    #region  Obj Part
+
+    public Image skin;
+    public GameObject check;
+
+    #endregion
+
+    public Knight k{get;set;}
+
+    void Start()
+    {
+        check.SetActive(false);
+    }
+
+
+    public void SetData(Knight _k)
+    {
+        k = _k;
+        skin.sprite = k.skin;
+    }
+
+    public void Check()
+    {
+        check.SetActive(check.activeSelf ? false : true);
+    }
+}
