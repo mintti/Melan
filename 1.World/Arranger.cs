211916ca806f7db64b99bf0452d2ce83e01cb73a
Mintti <mintti97@gmail.com>
2019-06-26 02:02:20 +0900

    드래그&드롭 코딩 연습.

---
+++ 1.World/Arranger.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arranger : MonoBehaviour
+{
+    List<Transform> children;
+    // Start is called before the first frame update
+    void Start()
+    {
+        children = new List<Transform>();
+
+        UpdateChildren();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateChildren()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            if(i == children.Count)
+            {
+                children.Add(null);
+            }
+
+            var child = transform.GetChild(i);
+
+            if(child != children[i])
+            {
+                children[i] = child;
+            }
+        }
+
+        children.RemoveRange(transform.childCount, children.Count - transform.childCount);
+    }
+
+    public void InsertKnight(Transform knight, int index)
+    {
+        children.Add(knight);
+        knight.SetSiblingIndex(index);
+        UpdateChildren();
+    }
+
+
+
+    public int GetIndexByPosition(Transform kngiht, int skipIndex = -1)
+    {
+        int result = 0;
+
+        for(int i =0 ; i < children.Count; i++)
+        {
+            if(kngiht.position.x < children[i].position.x)
+            {
+                break;
+            }
+            else if(skipIndex != i)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    public void SwapKnight(int index01, int index02)
+    {
+        Central.SwapKngihts(children[index01], children[index02]);
+    }
+}
---
+++ 1.World/Central.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Central : MonoBehaviour
+{
+    public Transform invisibleKnight;
+    
+    List<Arranger> arrangers;
+
+    Arranger workingArranger;
+    int originalIndex;
+
+    void Start()
+    {
+        arrangers = new List<Arranger>();
+
+        var arrs = transform.GetComponentsInChildren<Arranger>();
+
+        for(int i = 0; i < arrs.Length; i++)
+        {
+            arrangers.Add(arrs[i]);
+        }
+    }
+
+    public static void SwapKngihts(Transform sour, Transform dest)
+    {
+        Transform sourParent = sour.parent;
+        Transform destParent = dest.parent;
+
+        int sourIndex = sour.GetSiblingIndex();
+        int destIndex = dest.GetSiblingIndex();
+
+        sour.SetParent(destParent);
+        sour.SetSiblingIndex(destIndex);
+
+        dest.SetParent(sourParent);
+        dest.SetSiblingIndex(sourIndex);
+    }
+    
+    void SwapKngihtHierarchy(Transform sour, Transform dest)
+    {
+        SwapKngihts(sour, dest);
+
+        arrangers.ForEach(t => t.UpdateChildren());
+    }
+
+
+    bool ContainPos(RectTransform rt, Vector2 pos)
+    {
+        return RectTransformUtility.RectangleContainsScreenPoint(rt,pos);
+    }
+
+    void BeginDrag(Transform knight)
+    {
+        workingArranger = arrangers.Find(t=>ContainPos(t.transform as RectTransform, knight.position));
+        originalIndex = knight.GetSiblingIndex();
+
+        SwapKngihtHierarchy(invisibleKnight, knight);
+    }
+
+    void Drag(Transform knight)
+    {
+        var whichArrangerKnight = arrangers.Find(t=> ContainPos(t.transform as RectTransform, knight.position));
+        
+        if(whichArrangerKnight == null)
+        {
+            bool updateChildren = transform != invisibleKnight.parent;
+
+            invisibleKnight.SetParent(transform);
+
+            if(updateChildren)
+            {
+                arrangers.ForEach(t => t.UpdateChildren());
+            }
+        }
+        
+        else
+        {
+            bool insert = invisibleKnight.parent == transform;
+
+            if(insert)
+            {
+                int index = whichArrangerKnight.GetIndexByPosition(knight);
+
+                invisibleKnight.SetParent(whichArrangerKnight.transform);
+                whichArrangerKnight.InsertKnight(invisibleKnight, index );
+            }
+            else
+            {
+                int invisibleKnightIndex = invisibleKnight.GetSiblingIndex();
+                int targetIndex = whichArrangerKnight.GetIndexByPosition(knight, invisibleKnightIndex);
+                
+                if(invisibleKnightIndex != targetIndex)
+                {
+                    whichArrangerKnight.SwapKnight(invisibleKnightIndex, targetIndex);
+                }
+            }
+        }
+    }
+    void EndDrag(Transform knight)
+    {
+        if(invisibleKnight.parent == transform)
+        {
+            workingArranger.InsertKnight(knight, originalIndex);
+            workingArranger = null;
+            originalIndex = -1;
+        }
+        else
+        {
+            SwapKngihtHierarchy(invisibleKnight, knight);
+        }
+       
+    }
+}
