6a4efb908ca9ea84e4e17bee5def4f01eb9d16b8
Mintti <mintti97@gmail.com>
2019-06-27 23:56:10 +0900

    던전 선택 시 뜨는 Knight Prefab * 0 : 일반 선택 가능한 상태 * 1 : teaming (출전 상태) 인 상태 * 2 : selectView에 올라가 선택이 불가능한 상태.

--- 1.World/WorldKnightPrefab.cs
+++ 1.World/WorldKnightPrefab.cs
@@ -6,17 +6,28 @@ using UnityEngine.UI;
 public class WorldKnightPrefab : MonoBehaviour
 {
     #region  Obj Part
+    public WorldController world;
 
     public Image skin;
     public GameObject check;
-
+    public GameObject teaming;
     #endregion
 
     public Knight k{get;set;}
+    private int isType{get;set;} = 0;  // 0 : 선택가능 기사리스트 , 1 : 선택 불가 블럭 타입(현제 팀 출전중.),  2: 선택 시 제거됨(셀렉트뷰)
 
     void Start()
     {
         check.SetActive(false);
+        world = GameObject.FindWithTag("GameController").GetComponent<WorldController>();
+    }
+
+
+    //아래 출전기사들 리스트에 들어감.
+    public void KngihtInSelectView(Knight k)
+    {
+        SetData(k);
+        IsType(2);
     }
 
 
@@ -24,10 +35,50 @@ public class WorldKnightPrefab : MonoBehaviour
     {
         k = _k;
         skin.sprite = k.skin;
+
+        //출전 중인 기사들은..
+        if(k.teaming) 
+        {
+            IsType(1);
+        }
     }
 
+    //Obj Button 클릭시 호출됨.
     public void Check()
     {
-        check.SetActive(check.activeSelf ? false : true);
+        if(isType == 0)
+        {
+            check.SetActive(check.activeSelf ? false : true);
+
+            if(check.activeSelf)    world.AddKnightInParty(k);
+            else    world.RemoveKnightInParty(k);
+        }
+        else if(isType == 2)
+        {
+            world.RemoveKnightInParty(k);
+        }
     }
+
+    public void IsType(int i)
+    {
+        switch(i)
+        {
+            case 0 :
+                isType = 0;
+                check.SetActive(false);
+                //teaming.SetActive(false);
+                break;
+            case 1 : 
+                isType = 1;
+                teaming.SetActive(true);
+                break;
+            case 2 :
+                isType = 2;
+                break;
+            default :
+                break;
+        }
+    }
+
+
 }
