98f23128ba28806d1fc463bade92d42d8d78d9f2
Mintti <mintti97@gmail.com>
2019-07-09 23:04:30 +0900

    Player요소 추가.

---
+++ 7.Data(XML)/PlayerData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour
+{
+    #region PlayerData
+    public int gold;
+    public int day;
+    #endregion
+    
+    public Text goldText;
+    public Text dayText;
+
+
+    public void Test_InsertData()
+    {
+        gold = 100; 
+        day = 1;
+        UpdateText(); 
+    }
+
+    public void NextDay()
+    {
+        day++;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        dayText.text = string.Format("+{0}", day);
+        goldText.text = string.Format("{0}", gold);
+    }
+
+
+    
+}
