79e03cab3030a3bbc8dccc500856e81d0e5ed6d2
Mintti <mintti97@gmail.com>
2019-07-13 00:08:49 +0900

    Start씬 제작 * 게임 시작 시 화면임.

---
+++ 0.Game/StartController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    
+    public void Play()
+    {
+        SceneManager.LoadScene("1.Main");
+    }
+}
+    
