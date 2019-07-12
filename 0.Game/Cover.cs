859ea68740d828ee61d5aeb213460965a60eaf6f
Mintti <mintti97@gmail.com>
2019-07-13 00:08:31 +0900

    추후 데이터 로드 중 커버용.

---
+++ 0.Game/Cover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cover : MonoBehaviour
+{
+    Image img;
+
+    public void Play()
+    {
+        gameObject.SetActive(false);
+    }
+}
