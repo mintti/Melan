ecdf84bf634105704173a201541a36ddbd050b07
Mintti <mintti97@gmail.com>
2019-07-19 00:59:12 +0900

    no message

--- 7.Data(XML)/DungeonData.cs
+++ 7.Data(XML)/DungeonData.cs
@@ -3,31 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public class Dungeon : MonoBehaviour
+{
+    public int num{get;set;}
+    public string name{get;set;}
+    public int level{get;set;}
+    public int[] monsters{get;set;}
+    public int[] dangers{get;set;}
+    public int[] events{get;set;}
+    public int day{get;set;}
+    
+    //보상
+    public int gold{get;set;}
+    public int exper{get;set;}
+
+    public int before{get;set;}
+    public int next{get;set;}
+
+    public bool isClear{get;set;}
+
+
+    public bool isAdmit()
+    {
+        if(DungeonData.Instance.dungeons[before] == null || 
+            DungeonData.Instance.dungeons[before].isClear == true) return true;
+        else return false;
+    }
+   
+
+    public Dungeon(int _num, string _name, int _level, int[] _monster, int[] _dangers,
+        int[] _events, int _day, int _gold, int _exper, int _before, int _next, bool _isClear)
+    {
+        num = _num;
+        name = _name;
+        level = _level;
+        monsters = _monster;
+        dangers = _dangers;
+        events = _events;
+        day = _day;
+        gold = _gold;
+        exper = _exper;
+        before = _before;
+        next = _next;
+        isClear = _isClear;
+    }
+     
+}
+
 public class DungeonData : MonoBehaviour
 {
-    
-    public Transform Aragaya;
-    public Transform Benuxu;
+    private static DungeonData _instance = null;
 
-    public Dungeon[] dungeons;
-
-    public void Test_ConnectData()
+    public static DungeonData Instance
     {
-        int cnt = Aragaya.childCount + Benuxu.childCount;
-        Array.Resize(ref dungeons, cnt+1);
-
-        cnt = 1;
-
-        foreach(Transform tr in Aragaya)
+        get
         {
-            dungeons[cnt++] = tr.GetComponent<Dungeon>();
-        }
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(DungeonData)) as DungeonData;
 
-        foreach(Transform tr in Benuxu)
-        {
-            dungeons[cnt++] = tr.GetComponent<Dungeon>();
+                if(_instance == null)
+                {
+                    Debug.LogError("There's no active DungeonData object");
+                }
+            }
+            return _instance;
         }
     }
 
 
+    public static void InstanceUpdata()
+    {
+        _instance = null;
+    }
+
+    public Dungeon[] dungeons{get;set;}
+
+    public void ResizeDungeon(int size)
+    {
+        dungeons = new Dungeon[size];
+
+    }
+
+    public void Start()
+    {
+        TestValue();
+    }
+
+    public void TestValue()
+    {
+        for(int i = 0 ; i < dungeons.Length; i++)
+        {
+            Debug.Log(dungeons[i].name);
+        }
+    }
+    
+    
 }
