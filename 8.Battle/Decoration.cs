7a300a440e5d3e4bfed4f90a4fc7439484fd0287
Mintti <mintti97@gmail.com>
2019-09-21 23:25:32 +0900

    추후 사용

--- 8.Battle/Decoration.cs
+++ 8.Battle/Decoration.cs
@@ -14,15 +14,31 @@ public class DungeonTheme
 
 public class Decoration : MonoBehaviour
 {
+    private static Decoration _instance = null;
+
+    public static Decoration Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(Decoration)) as Decoration;
+
+                if(_instance == null)
+                {
+                    Debug.LogError("There's no active Decoration object");
+                }
+            }
+            return _instance;
+        }
+    }
+
+
     public DungeonTheme[] theme;
 
     //현제 오브젝트
     public SpriteRenderer bg;
     public SpriteRenderer ground;
 
-    public void SetTheme(int num)
-    {
-        bg.sprite = theme[num].bg;
-        ground.sprite = theme[num].ground;
-    }
+    
 }
