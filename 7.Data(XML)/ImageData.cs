b0daff94089bb560171d8c06357b84948c520143
Mintti <mintti97@gmail.com>
2019-09-21 23:26:15 +0900

    Icon 이미지를 저장하는 스크립트. * GetSprite(string)를 통해 원하는 아이콘을 호출함.

---
+++ 7.Data(XML)/ImageData.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageData : MonoBehaviour
+{
+    private static ImageData _instance = null;
+
+    public static ImageData Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(ImageData)) as ImageData;
+
+                if(_instance == null)
+                {
+                    Debug.LogError("There's no active ImageData object");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public Sprite targetSolo;
+    public Sprite targetMulti;
+    
+    public Sprite blood;
+    public Sprite stun;
+    public Sprite poison;
+    public Sprite ele;
+
+
+    public Sprite GetSprite(string type)
+    {
+        switch(type)
+        {
+            case "타겟솔로" : return targetSolo;
+            case "타겟멀티" : return targetMulti;
+            case "기절" : return stun;
+            case "출혈" : return blood;
+            case "독" : return poison;
+            case "속성" : return ele;
+            default :
+                CodeBox.PrintError("ImageData - GetSprite () 유형 없음");
+                return null;
+        }
+    }
+    
+  
+}
