d6eda8e6ee487ba0d843e283d614401984460c2c
Mintti <mintti97@gmail.com>
2019-09-21 23:33:40 +0900

    Battle - Skill 구성 * 스킬의 정보를 읽어 화면에 표시해줌. * 스킬 소모코스트, 필요 속성 등등의 여부를 표시하게 해줌(ing)

---
+++ 8.Battle/CCIconPrefab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CCIconPrefab : MonoBehaviour
+{
+    public Text num;
+    public Image icon;
+
+    public void SetData(string type, int num)
+    {
+        Sprite s = ImageData.Instance.GetSprite(type);
+        
+        icon.sprite = s;
+        this.num.text = string.Format("{0}" , num);
+    }
+}
