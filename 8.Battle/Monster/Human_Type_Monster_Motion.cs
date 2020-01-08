2f078dc4371082970fca7679469d941f04149966
Mintti <mintti97@gmail.com>
2020-01-09 00:30:58 +0900

    인간형 몬스터 틀 작성. 간단한 공통 애니메이션 제작함.

---
+++ 8.Battle/Monster/Human_Type_Monster_Motion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human_Type_Monster_Motion : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine("BlinkEyes");
+    }
+
+    public void Action(string action)
+    {
+        animator.Play(action);
+    }
+    IEnumerator BlinkEyes()
+    {
+        while(true)
+        {
+            float time = Random.Range(5f, 10f);
+            yield return new WaitForSeconds(time);
+            animator.Play("EyesMotion.CloseEyes");
+        }
+    }
+}
