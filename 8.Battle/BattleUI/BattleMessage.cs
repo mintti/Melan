b884ea1b71a02eb8670d3ab31aca4911e38d184d
Mintti <mintti97@gmail.com>
2020-05-16 02:06:58 +0900

    게임의 진행 메세지의 스크립트

---
+++ 8.Battle/BattleUI/BattleMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMessage : MonoBehaviour
+{
+    private void Start() {
+        gameObject.SetActive(false);
+    }
+    public void SendMessage(string text)
+    {
+        gameObject.SetActive(true); 
+        GetComponentInChildren<TypingText>().SetData(transform, text);
+    }
+    public void Next()
+    {
+        StartCoroutine("Wait");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+    }
+}
